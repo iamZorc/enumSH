ae2e53c0037e342ca6b9506fa4b95617fd0494da
y0ussefelgohre <118435243+y0ussefelgohre@users.noreply.github.com>
2025-11-21 19:06:26 +0000

    script file + DNS resolvers file

---
+++ enum_edit.sh
@@ -0,0 +1,151 @@
+#!/bin/bash
+
+if [ ! -f resolvers.txt ]; then
+    echo "no resolvers file found, exiting."
+    echo "use this URL to find resolvers -> https://github.com/trickest/resolvers"
+    exit 1
+fi
+
+echo -n "enter target name (in this format -> target.TLD): "
+read domain
+
+mkdir -p "$domain"
+cd "$domain"
+
+echo ""
+
+echo "running findomain, assetfinder, subfinder
+
+findomain -t "$domain" -u subdomains1.txt &
+$HOME/go/bin/assetfinder --subs-only "$domain" > subdomains2.txt &
+$HOME/go/bin/subfinder -d "$domain"  -config ~/.config/subfinder/config.yaml -o subdomains3.txt &
+
+wait
+
+if [ ! -s subdomains1.txt ] && [ ! -s subdomains2.txt ] && [ ! -s subdomains3.txt ]; then
+   echo "subdomain file is empty, exiting."
+   rm -f subdomains1.txt subdomains2.txt subdomains3.txt
+   exit 1
+else
+   cat subdomains1.txt subdomains2.txt subdomains3.txt | sort -u -o subdomains.txt
+   rm -f subdomains1.txt subdomains2.txt subdomains3.txt
+fi
+
+echo "step 1 done [findomain, assetfinder, subfinder]"
+
+echo "running alterx"
+
+$HOME/go/bin/alterx -l subdomains.txt -en -o permutations.txt
+
+if [ ! -s permutations.txt ]; then
+    echo "no permutations generated"
+    rm -f permutations.txt
+else
+    echo "running dnsx to get dead subdomains"
+    cat subdomains.txt permutations.txt | sort -u > all_domains.txt
+    $HOME/go/bin/dnsx -l all_domains.txt -r ../resolvers.txt -json -o master_dns.json
+    cat master_dns.json | jq -r 'select(.status_code == "NXDOMAIN" or .status_code == "SERVFAIL" or .status_code == "REFUSED") | .host' | sort -u > dead_subdomains.txt
+    	if [ ! -s dead_subdomains.txt ]; then
+            echo "no dead subdomains found"
+            rm -f dead_subdomains.txt
+	else
+    	    echo "dead subdomains found"
+    	    echo "step 2 done [dnsx]"
+	fi
+    echo "step 3 done [alterx]"
+fi
+
+echo "running dnsx to get subdomains that resolve"
+
+cat master_dns.json | jq -r 'select(.status_code == "NOERROR") | .host' | sort -u > resolved_subdomains.txt
+
+if [ ! -s resolved_subdomains.txt ]; then
+    echo "no resolved subdomains found, exiting."
+    rm -f resolved_subdomains.txt
+    exit 1
+else
+    sort -u resolved_subdomains.txt -o resolved_subdomains.txt
+    echo "step 4 done [dnsx]"
+fi
+
+echo "running httpx"
+
+$HOME/go/bin/httpx -l resolved_subdomains.txt -mc 200 -timeout 3 -o 200_OK_subdomains.txt
+
+if [ ! -s 200_OK_subdomains.txt ]; then
+    echo "no 200 OK subdomains found"
+    rm -f 200_OK_subdomains.txt
+    exit 1
+else
+    sort -u 200_OK_subdomains.txt -o 200_OK_subdomains.txt
+    echo "step 5 done [httpx]"
+fi
+
+echo "running dnsx again to get IPs for live subdomains"
+
+grep -Ff 200_OK_subdomains.txt master_dns.json | jq -r 'select(.a != null) | .a[]' | sort -u > alive_IPs.txt
+
+if [ ! -s alive_IPs.txt ]; then
+    echo "no IPs found for live subdomains"
+    rm -f alive_IPs.txt
+else
+    echo "step 6 done [dnsx]"
+fi
+
+echo "running VhostFinder"
+
+$HOME/go/bin/VhostFinder -ips alive_IPs.txt -wordlist dead_subdomains.txt -verify > VhostFinder_results.txt
+
+if [ ! -s VhostFinder_results.txt ]; then
+    echo "no Vhosts found"
+    rm -f Vhosts.txt
+else
+    echo "step 7 done [VhostFinder]"
+fi
+
+echo "running gau"
+
+cat 200_OK_subdomains.txt | $HOME/go/bin/gau --o gau_results.txt --threads 5 --blacklist png,jpg,svg,jpeg,
+
+if [ ! -s gau_results.txt ]; then
+    echo "no results found with gau"
+    rm -f gau_results.txt
+else
+    echo "step 8 done [gau]"
+    echo "running arjun"
+    arjun -i gau_results.txt -oT arjun_results.txt --stable --disable-redirects
+    if [ ! -s arjun_results.txt ]; then
+        echo "no results found with arjun"
+        rm -f arjun_results.txt
+    else
+        echo "step 9 done [arjun]"
+        echo "running kxss"
+        cat arjun_results.txt | $HOME/go/bin/kxss > kxss_results.txt
+        if [ ! -s kxss_results.txt ]; then
+            echo "no results found with kxss"
+            rm -f kxss_results.txt
+        else
+            echo "step 10 done [kxss]"
+        fi
+    fi
+fi
+
+echo "running dnsx again to get subdomains that has CNAME DNS records from the resolved subdomains"
+
+cat master_dns.json | jq -r 'select(.cname != null) | .host' | sort -u > CNAME_subdomains.txt
+
+if [ ! -s CNAME_subdomains.txt ]; then
+    echo "no subdomains with CNAME DNS record found, exiting."
+    rm -f CNAME_subdomains.txt
+    exit 1
+else
+   echo "step 11 done [dnsx]"
+   echo "running subzy"
+   $HOME/go/bin/subzy run --hide_fails --targets CNAME_subdomains.txt >> subdomain_takeover.txt
+    if [ ! -s subdomain_takeover.txt ]; then
+        echo "no subdomain takeover found"
+        rm -f subdomain_takeover.txt
+    else
+        echo "step 12 done [subzy]"
+    fi
+fi
