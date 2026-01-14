8776aeb402855080168aed80d66f3b290869fe5f
iamZorc <118435243+iamZorc@users.noreply.github.com>
2026-01-14 19:38:56 +0200

    Add amass command for subdomain enumeration

--- enum.sh
+++ enum.sh
@@ -36,6 +36,7 @@ assetfinder --subs-only "$domain" > subdomains2.txt &
 subfinder -d "$domain"  -all -recursive -config ~/.config/subfinder/config.yaml -o subdomains3.txt &
 github-subdomains -d "$domain" -t "$github_token" -o subdomains4.txt &
 curl -H "x-api-key: "$subbdom_token"" "https://api.subbdom.com/v1/search?z=$domain" | jq -r '.[]' > subdomains5.txt &
+amass enum -d "$domain" -o output.txt &
 
 wait
 
